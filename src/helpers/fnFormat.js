32f7791099581e44e03cdc147155a919eeece556
jseniuk <jseniuk@acromedia.com>
2021-01-04 20:29:38 -0800

    Versatility

--- src/helpers/fnFormat.js
+++ src/helpers/fnFormat.js
@@ -1,6 +1,7 @@
 module.exports = (fn, args) => {
   const fnString = String(fn);
   let fnArgs = fnString.split('(')[1].split(')')[0].split(',');
+  let internal;
 
   fnArgs = fnArgs.map(a => {
     let val = args[fnArgs.indexOf(a)] || null;
@@ -10,8 +11,15 @@ module.exports = (fn, args) => {
     return `${a}=${val}`;
   });
 
-  const internal = fnString.split(/\{(.+)/)[1].slice(0, -1);
+  if (fnString.split(/\{/)[1]) {
+    internal = fnString.split(/\{/)[1].slice(0, -1);
+  } else {
+    internal = fnString.split('=> ')[1] || null;
+  }
+
   const final = `((${fnArgs.join(',')}) => { ${internal} })()`;
+
+  console.log(final);
 
   return final;
 };
