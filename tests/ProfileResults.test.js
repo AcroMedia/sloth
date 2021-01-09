cbea5b1c172650ac94ec712ff8707c26c79f38c9
jseniuk <jseniuk@acromedia.com>
2021-01-08 20:27:41 -0800

    ProfileResults testing

---
+++ tests/ProfileResults.test.js
@@ -0,0 +1,41 @@
+const ProfileResults = require('../src/classes/ProfileResults')
+
+const mockData = {
+  start: Date.now(),
+  end: Date.now() + 1200,
+  time_elapsed: 1200,
+  timestep_ms: 100,
+  mem_list: [
+    32690176,
+    32456704,
+    32456704,
+    32690176,
+    32690176,
+    32690176,
+    32690176,
+    32690176,
+    32690176,
+    32690176,
+    32690176,
+    32690176
+  ],
+  start_usage_bytes: 32456704,
+  peak_usage_bytes: 32690176,
+  end_usage_bytes: 32690176,
+  base_process_bytes: 32456704,
+}
+
+describe('ProfileResults', () => {
+  it('tests averaging functions', () => {
+    const results = new ProfileResults(mockData)
+
+    expect(results.averageMemoryUsage() === 32651264).toBeTruthy()
+    expect(results.medianMemoryUsage() === 32456704).toBeTruthy()
+    expect(results.modeMemoryUsage() === 32690176).toBeTruthy()
+
+    expect(results.memoryAtElapsed(200) === 32456704).toBeTruthy()
+    expect(results.memoryAtElapsed(800) === 32690176).toBeTruthy()
+
+    expect(() => results.memoryAtElapsed(999999)).toThrow()
+  })
+})
