c6a14a60074fc07eca118bd44139de8ab7559142
jseniuk <jseniuk@acromedia.com>
2021-01-11 16:47:59 -0800

    Split docs from readme

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ module.exports = {
   Profiler: require('./src/classes/Profiler'),
   bench: require('./src/functions/bench')
 };
+
+/**
+ * TODO: FIX FUNCTION PARSING SINCE IT GOES MESSY WHEN THERE ARE {} WITHIN THE FUNCTION
+ */
