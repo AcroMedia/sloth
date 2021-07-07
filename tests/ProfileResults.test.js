66e37200f406c354e5c6d3aee5c9256e4a1cc542
jseniuk <jseniuk@acromedia.com>
2021-07-07 13:56:57 -0700

    WIP snapshot testing

--- tests/ProfileResults.test.js
+++ tests/ProfileResults.test.js
@@ -1,5 +1,3 @@
-const ProfileResults = require('../src/classes/ProfileResults');
-
 const mockData = {
   start: Date.now(),
   end: Date.now() + 1200,
@@ -25,6 +23,15 @@ const mockData = {
   base_process_bytes: 32456704
 };
 
+jest.mock('fs', () => ({
+  existsSync: jest.fn(() => true),
+  mkdirSync: jest.fn(),
+  writeFileSync: jest.fn((path, data) => ({ path, data })),
+  readFileSync: jest.fn(path => JSON.stringify(mockData))
+}));
+
+const ProfileResults = require('../src/classes/ProfileResults');
+
 describe('ProfileResults', () => {
   it('tests class functions', () => {
     const results = new ProfileResults(mockData);
@@ -37,5 +44,25 @@ describe('ProfileResults', () => {
     expect(results.memoryAtElapsed(800) === 32690176).toBeTruthy();
 
     expect(() => results.memoryAtElapsed(999999)).toThrow();
+  });
+
+  it('tests snapshotting', () => {
+    const results = new ProfileResults(mockData);
+
+    expect(() => results.saveSnapshot()).toThrow();
+
+    const savedData = results.saveSnapshot('name', '/home/snapshots/');
+    const parsed = JSON.parse(savedData.data);
+
+    expect(savedData.path).toBe('/home/snapshots/name.json');
+
+    // Data should essentially be duplicated
+    Object.keys(mockData).forEach((key) => {
+      expect(parsed[key]).toStrictEqual(mockData[key]);
+    });
+  });
+
+  it('tests snapshot comparison', () => {
+    const results = new ProfileResults(mockData);
   });
 });
