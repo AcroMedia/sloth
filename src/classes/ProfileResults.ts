892f69a21b70bdad66f75629ae4c944e1bd186d1
jseniuk <jseniuk@acromedia.com>
2021-07-12 16:33:09 -0700

    TS-ify ProfileResults

--- src/classes/ProfileResults.ts
+++ src/classes/ProfileResults.ts
@@ -1,10 +1,24 @@
-require('colors');
+const { red, green } = require('colors');
 const fs = require('fs');
 const _path = require('path');
 const asciichart = require('asciichart');
 const createChart = require('../helpers/createChart');
 
+
 export default class ProfileResults {
+  private data: {
+    start: number,
+    end: number,
+    time_elapsed: number,
+    timestep_ms: number,
+    mem_list: Array<number>,
+    start_usage_bytes: number,
+    peak_usage_bytes: number,
+    end_usage_bytes: number,
+    base_process_bytes: number,
+    last_updated: string
+  };
+
   /**
    * Wraps the results of the Profiler into a class.
    *
@@ -13,37 +27,8 @@ export default class ProfileResults {
    *
    * It is also helpful for providing useful jsdoc
    * definitions for the properties of the data.
-   *
-   * @param {Object} data
-   *
-   * @param {Number} data.start
-   * When the profiling started.
-   *
-   * @param {Number} data.end
-   * When the profiling was stopped.
-   *
-   * @param {Number} data.time_elapsed
-   * The amount of time the profiler was active, in milliseconds.
-   *
-   * @param {Number} data.timestep_ms
-   * The amount of milliseconds that would pass before each memory check.
-   *
-   * @param {Array} data.mem_list
-   * The list of memory values throughout the test.
-   *
-   * @param {Number} data.start_usage_bytes
-   * The amount of bytes that the process took up when profiler started.
-   *
-   * @param {Number} data.peak_usage_bytes
-   * The highest amount of memory usage.
-   *
-   * @param {Number} data.end_usage_bytes
-   * The amount of bytes the process took up when the profile was finished.
-   *
-   * @param {Number} data.base_process_bytes
-   * The amount of bytes the process took up without anything happening.
    */
-  constructor (data) {
+  constructor (data: any) {
     this.data = data;
   }
 
@@ -51,8 +36,8 @@ export default class ProfileResults {
    * Get average memory usage.
    */
   averageMemoryUsage () {
-    let total = 0;
-    this.data.mem_list.forEach((m) => {
+    let total: number = 0;
+    this.data.mem_list.forEach((m: number) => {
       total += m;
     });
 
@@ -63,14 +48,14 @@ export default class ProfileResults {
    * Get median memory usage.
    */
   medianMemoryUsage () {
-    return this.data.mem_list.sort((a, b) => a > b)[Math.round(this.data.mem_list.length / 2)];
+    return this.data.mem_list.sort((a, b) => a - b)[Math.round(this.data.mem_list.length / 2)];
   }
 
   /**
    * Get most frequently occuring value in memory usage.
    */
   modeMemoryUsage () {
-    const freqMap = {};
+    const freqMap: any = {};
     let maxCount = 0;
     let largest = null;
 
@@ -92,7 +77,7 @@ export default class ProfileResults {
    *
    * @param {Number} ms
    */
-  memoryAtElapsed (ms) {
+  memoryAtElapsed (ms: number) {
     if (ms > this.data.time_elapsed) throw new Error('Time provided was greater than total profile time.');
 
     return this.data.mem_list[Math.round(ms / this.data.timestep_ms)];
@@ -100,11 +85,8 @@ export default class ProfileResults {
 
   /**
    * Save data as a snapshot for comparison in future tests.
-   *
-   * @param {String} filename
-   * @param {String} path
    */
-  saveSnapshot (filename, path = `${_path.resolve('.')}/__snapshots__/`) {
+  saveSnapshot (filename: string, path = `${_path.resolve('.')}/__snapshots__/`) {
     if (!filename) throw Error('You must provide a file name.');
 
     if (!fs.existsSync(path)) {
@@ -126,7 +108,11 @@ export default class ProfileResults {
    * @param {String=} options.graph
    * @param {String=} options.graph_path
    */
-  compareToSnapshot (path, options = {}) {
+  compareToSnapshot (path: string, options: {
+    logResultsDiff?: boolean,
+    graph?: string,
+    graph_path?: string
+  } = {}) {
     let obj;
 
     try {
@@ -146,7 +132,7 @@ export default class ProfileResults {
       };
     }
 
-    const comparison = {
+    const comparison: any = {
       time_elapsed: this.data.time_elapsed - obj.time_elapsed,
       start_usage_bytes: this.data.start_usage_bytes - obj.start_usage_bytes,
       peak_usage_bytes: this.data.peak_usage_bytes - obj.peak_usage_bytes,
@@ -155,17 +141,17 @@ export default class ProfileResults {
 
     if (options.logResultsDiff) {
       Object.keys(comparison).forEach((k) => {
-        if (k === 'time_elapsed') return console.log(`Time elapsed: ${comparison[k] > 0 ? `+${comparison[k]}ms`.red : `${comparison[k]}ms`.green}`);
-        return console.log(`${(k[0].toUpperCase() + k.substr(1)).replace(/_/g, ' ')}: ${comparison[k] > 0 ? `+${bytes(comparison[k])}`.red : `-${bytes(-comparison[k])}`.green}`);
+        if (k === 'time_elapsed') return console.log(`Time elapsed: ${comparison[k] > 0 ? red(`+${comparison[k]}ms`) : green(`${comparison[k]}ms`)}`);
+        return console.log(`${(k[0].toUpperCase() + k.substr(1)).replace(/_/g, ' ')}: ${comparison[k] > 0 ? `+${red(bytes(comparison[k]))}` : `-${green(bytes(-comparison[k]))}`}`);
       });
     }
 
     if (options.graph) {
-      const chartData = [this.data.mem_list.map(n => n / 1024), obj.mem_list.map(n => n / 1024)];
+      const chartData = [this.data.mem_list.map(n => n / 1024), obj.mem_list.map((n: number) => n / 1024)];
 
       if (options.graph === 'text') {
         // Graph memory chart
-        console.log(asciichart.plot(chartData.sort((a, b) => a.length > b.length), {
+        console.log(asciichart.plot(chartData.sort((a, b) => a.length - b.length), {
           height: 10,
           colors: [
             this.data.mem_list.length > obj.mem_list.length ? asciichart.red : asciichart.blue,
@@ -186,18 +172,18 @@ export default class ProfileResults {
   }
 };
 
-function bytes (val) {
+function bytes (val: number) {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
 
   if (val === 0) {
     return '0 Bytes';
   }
 
-  const i = parseInt(Math.floor(Math.log(val) / Math.log(1024)), 10);
+  const i: number = Math.floor(Math.log(val) / Math.log(1024));
 
   if (i === 0) {
     return `${val} ${sizes[i]}`;
   }
 
-  return `${val / (1024 ** i).toFixed(2)} ${sizes[i]}`;
+  return `${val / parseFloat((1024 ** i).toFixed(2))} ${sizes[i]}`;
 }
