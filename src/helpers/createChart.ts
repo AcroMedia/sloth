016bb9d538f988fb2842a32f66468c927a3a4694
jseniuk <jseniuk@acromedia.com>
2021-07-26 15:44:54 -0700

    Begin converting d3 charting

--- src/helpers/createChart.ts
+++ src/helpers/createChart.ts
@@ -13,7 +13,7 @@ export default (data: Array<Array<number>>, path: string): void => {
   const w = 900 - margin.left - margin.right;
   const h = 500 - margin.top - margin.bottom;
 
-  let svg: Selection<unknown> = d3.select(dom.window.document).select('body')
+  let svg: d3.Selection<SVGSVGElement, unknown, null, undefined> = d3.select(dom.window.document).select('body')
     .append('svg')
     .attr('width', w + margin.left + margin.right)
     .attr('height', h + margin.top + margin.bottom);
@@ -24,22 +24,20 @@ export default (data: Array<Array<number>>, path: string): void => {
     .attr('width', '100%')
     .attr('height', '100%');
 
+  // @ts-expect-error The SVG type is goofy in that there are like 3 different SVGElement types
   svg = svg.append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
   // Data mapping
-  const chartData: { x: number, y: number }[][] = data.map(
+  const chartData: [number, number][][] = data.map(
     (d: Array<number>) => d.map(
-      (val: number, i) => ({
-        x: i,
-        y: val,
-      }),
+      (val: number, i) => ([i, val]),
     ),
   );
 
   // Get the largest of the two arrays so we can scale the graph properly
-  const largest: { x: number, y: number }[] = chartData.sort(
-    (a: { x: number, y: number }[], b: { x: number, y: number }[]) => a.length - b.length,
+  const largest: [number, number][] = chartData.sort(
+    (a: [number, number][], b: [number, number][]) => a.length - b.length,
   )[0];
 
   // Dimension functions, used for data scaling
@@ -47,19 +45,21 @@ export default (data: Array<Array<number>>, path: string): void => {
   const y: d3.ScaleLinear<number, number> = d3.scaleLinear().range([h, 0]);
 
   // Use scaling functions from before to create a new line creating function
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  const lineFunc: Function = d3.line()
+  const lineFunc: d3.Line<[number, number]> = d3.line()
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     .x((d: any) => x(d.x))
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     .y((d: any) => y(d.y))
     .curve(d3.curveLinear);
 
-  x.domain(d3.extent(largest, (d) => d.x));
-  y.domain([0, d3.max(largest, (d) => d.y)]);
+  // @ts-expect-error
+  x.domain(d3.extent(largest, (d: { x: number }) => d.x));
+  // @ts-expect-error
+  y.domain([0, d3.max(largest, (d: { y: number }) => d.y)]);
 
   // First path
   svg.append('svg:path')
+  // @ts-expect-error
     .attr('d', lineFunc(chartData[0]))
     .attr('stroke', 'blue')
     .attr('stroke-width', 2)
@@ -67,6 +67,7 @@ export default (data: Array<Array<number>>, path: string): void => {
 
   // Second (shorter if not the same) path
   svg.append('svg:path')
+  // @ts-expect-error
     .attr('d', lineFunc(chartData[1]))
     .attr('stroke', 'red')
     .attr('stroke-width', 2)
