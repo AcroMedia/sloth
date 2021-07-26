993d3fadaa761fb75b3198ae886ae453de679a9b
jseniuk <jseniuk@acromedia.com>
2021-07-26 15:51:55 -0700

    Update tsc

--- index.js
+++
@@ -1,9 +0,0 @@
-import Profiler from './dist/classes/Profiler';
-import bench from './dist/functions/bench';
-import benchFile from './dist/functions/benchFile';
-
-export default {
-  Profiler,
-  bench,
-  benchFile,
-};
