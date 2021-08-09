9150ecfdb3ef1ef4ccd910582a23be2cde138ca1
jseniuk <jseniuk@acromedia.com>
2021-08-09 09:26:42 -0700

    Remove non-vital tests

--- tests/jest/Profiler.spec.ts
+++ tests/jest/Profiler.spec.ts
@@ -23,31 +23,3 @@ describe('small data tests', () => {
     expect(prof.wait).toBe(1000);
   });
 });
-
-jest.setTimeout(20000);
-
-describe('large data tests', () => {
-  it('ensures data consistancy with big data', async () => {
-    const prof = new Profiler(process.pid, {
-      timestep: 100,
-      toFile: false,
-      waitAfterEnd: 1000,
-      trimNodeProcessUsage: true,
-    });
-
-    // This can take a lil while.
-    jest.setTimeout(60000);
-
-    await prof.start();
-
-    // One hundred million 0s.
-    const myHugeArray = new Array(1e8).fill(0);
-    myHugeArray.reverse();
-
-    const results: ProfileResults = await prof.end();
-    const peak = results.data.peak_usage_bytes / (1000 * 1000);
-
-    expect(peak).toBeGreaterThan(1190);
-    expect(peak).toBeLessThan(1400);
-  });
-});
